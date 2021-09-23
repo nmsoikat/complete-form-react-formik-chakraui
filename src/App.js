f85d0d271a61f185137f4bdd1636964f29e3fcc4
saykat1 <nursaykat@gmail.com>
2021-09-23 15:51:46 +0600

    textfield

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.css';
-import FormikContainerNF from './components/normalFields/FormikContainerNF';
-import Container from '@mui/material/Container';
-import FormikContainer from './components/FormikContainer';
+import "./App.css";
+import FormikContainerNF from "./components/normalFields/FormikContainerNF";
+import Container from "@mui/material/Container";
+import FormikContainer from "./components/FormikContainer";
+import { ThemeProvider } from "@mui/styles";
+import { CssBaseline } from "@mui/material";
 
 function App() {
-	return (
-		<main>
-			<Container sm="sm">
-				<FormikContainer />
-			</Container>
-			{/* <FormikContainerNF /> */}
-		</main>
-	);
+  return (
+    <main>
+      <Container sm="sm">
+        <FormikContainer />
+      </Container>
+      {/* <FormikContainerNF /> */}
+      <CssBaseline />
+    </main>
+  );
 }
 
 export default App;
--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -1,21 +1,43 @@
-import { Box, Grid } from '@mui/material';
-import { borderRadius } from '@mui/system';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
+import { Grid, TextField, Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+const useStyles = makeStyles((theme) => ({
+  formContainer: {
+    display: "block",
+    border: "1px solid #ddd",
+    borderRadius: "15px",
+    padding: "15px",
+
+    "& .MuiFormControl-root": {
+      width: "100%",
+      margin: "8px",
+    },
+  },
+}));
 
 function FormikContainer() {
-	return (
-		<Grid container spacing={4} alignItems="center" minHeight="100vh">
-			<Grid item container md={8}>
-				<img src="img/login-bg.jpg" alt="login" width="100%" />
-			</Grid>
-			<Grid item container md={4}>
-				<form>
-					<input type="text" />
-				</form>
-			</Grid>
-		</Grid>
-	);
+  const classes = useStyles();
+
+  return (
+    <Grid container alignItems="center" minHeight="100vh">
+      <Grid item container md={6}>
+        <img src="img/login-bg.jpg" alt="login" width="100%" />
+      </Grid>
+      <Grid item container md={6}>
+        <form className={classes.formContainer}>
+          <Box sx={{ display: "flex" }}>
+            <TextField label="First Name" name="fname" />
+            <TextField label="Last Name" name="lname" />
+          </Box>
+          <Box sx={{ display: "flex" }}>
+            <TextField type="email" label="Email" />
+          </Box>
+        </form>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default FormikContainer;
